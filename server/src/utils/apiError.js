d2147e107b4dde07444151bd850404d560d3d5f5
nitinsharma1845 <nitinsharma992740@gmail.com>
2025-08-15 23:17:59 +0530

    feat: add ApiError, ApiResponse, and asyncHandler utility classes

---
+++ server/src/utils/apiError.js
@@ -0,0 +1,8 @@
+class ApiError {
+    constructor(statusCode , message="Something went wrong"){
+        this.statusCode = statusCode
+        this.message = message
+    }
+}
+
+export {ApiError}
---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// const asyncHandler = (fn) =>{
+//     return (req , res , next) =>{
+//         Promise.resolve(fn(req , res , next)).catch(next)
+//     }
+// }
+//if we use it we have to add a middleware to handle the rror and send the response accordingly
+
+const asyncHandler = async (fn) =>{
+    try {
+        return await fn(req , res , next)
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            message : error.message || "Internal Server Error ::: ",
+            sucess : false
+        })
+    }
+}
+
+
+export {asyncHandler}
